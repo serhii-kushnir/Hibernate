c86854569e9039f9ef5f53365ddb4281588885cb
fear1611 <127629681+fear1611@users.noreply.github.com>
2023-10-19 15:21:12 +0300

    mod13-task1 Done

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class App {
+
+    public static void main(String[] args) {
+        log.info("Started program");
+
+        log.info("The program is completed");
+    }
+}
+
